1640e3d4b7f483954671f5f9d1b11e0673edb346
moke_zeru <mokemz24@gmail.com>
2018-07-16 15:49:51 -0500

    Transfer Commit!

---
+++ src/app/bankmodule/transfer.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { LocalService } from './services/local.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-transfer',
+  template: `
+   <div>
+    <form>
+      Amount <input type="text" #amt (input)="transferAmount=amt.value"><br><br>
+      Beneficiary Acct. <input type="text" #act ><br><br>
+      <button type="submit" (click)="transfer(amt.value,act.value)">Transfer</button>
+
+    </form>
+   </div>
+  `,
+  styles: []
+})
+export class TransferComponent implements OnInit {
+
+  constructor(private ls:LocalService,private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  transfer(amt,act){
+    this.ls.transferFund(amt,act).subscribe(res=>{
+      if(res.message!=null){
+        this.router.navigate(['transferconfirmation']);
+      }
+      else if(res.error=='Insufficient Balance'){
+        this.router.navigate(['transfererror']);
+      }
+      else{
+        this.router.navigate(['error']);
+      }
+    })
+  }
+
+}
